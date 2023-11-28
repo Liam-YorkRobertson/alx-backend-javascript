a9ab9f52126b6cacf9400396bd6436f4a782d0dc
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-11-28 02:45:25 +0200

    working with variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-const-assign
     task = true;
+    // eslint-disable-next-line no-const-assign
     task2 = false;
   }
 
