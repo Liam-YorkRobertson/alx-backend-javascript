4563b1d8c332ecfe0ff29bf584b461d445567475
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-11-30 23:32:07 +0200

    using classes, getters

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    } else {
+      this._sqft = sqft;
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
