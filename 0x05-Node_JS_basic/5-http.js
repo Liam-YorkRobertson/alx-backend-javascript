ddedff4768be54e75a3788b6b70c7da5047f2c25
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-31 00:27:11 +0200

    created small more complex http server using node http module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,22 +1,36 @@
 // more complex small http server using node
 
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const { readFile } = require('fs').promises;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then(() => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('This is the list of our students\n');
-      });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
+    const data = await readFile(process.argv[2], 'utf-8');
+    const { length, fields, students } = processStudentData(data);
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`This is the list of our students\nNumber of students: ${length}\n${fields}`);
   }
 });
+
+function processStudentData(data) {
+  const students = {};
+  let length = 0;
+  data.trim().split('\n').forEach((line, index) => {
+    if (index !== 0) {
+      length += 1;
+      const [firstname, lastname, age, field] = line.split(',').map((value) => value.trim());
+      students[field] = (students[field] || []).concat(firstname);
+    }
+  });
+  const fields = Object.entries(students).map(([key, value]) => {
+    const studentsCount = value.length;
+    const studentList = value.join(', ');
+    return `Number of students in ${key}: ${studentsCount}. List: ${studentList}`;
+  });
+  return { length, fields: fields.join('\n') };
+}
 
 app.listen(1245);
 
