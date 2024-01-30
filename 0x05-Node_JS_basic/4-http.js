0614e2675c2a2ed216a36ae67c26179a29876bba
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-30 23:34:13 +0200

    created small http server using node http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
