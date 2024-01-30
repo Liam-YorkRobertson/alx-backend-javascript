27b3abb8e015116280c8aaadcd118aae5dfc8cdb
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-30 22:08:45 +0200

    takes input and displys an output

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// takes input and serves output
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
