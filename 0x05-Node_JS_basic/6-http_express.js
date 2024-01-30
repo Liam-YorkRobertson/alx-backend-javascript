9e8cadd741f636052b9b8fd2ff145ce847cb0096
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-31 01:14:21 +0200

    small http server using express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+// small http server using express
+
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+});
+
+module.exports = app;
