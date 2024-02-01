9f56390db3aa3babea46cfaea8550a56c05e54ac
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-02-01 20:04:06 +0200

    using sinon spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,7 +2,7 @@
 
 const { describe, it } = require('mocha');
 const sinon = require('sinon');
-const expect = require('chai');
+const assert = require('assert');
 const Utils = require('./utils');
 const paymentModule = require('./3-payment');
 
@@ -10,7 +10,7 @@ describe('sendPaymentRequestToApi', () => {
   it('validate usage of Utils.utils', () => {
     const spy = sinon.spy(Utils, 'utils');
     paymentModule.sendPaymentRequestToApi(100, 20);
-    expect(spy.calledWith('SUM', 100, 20));
+    assert(spy.calledWith('SUM', 100, 20));
     spy.restore();
   });
 });
